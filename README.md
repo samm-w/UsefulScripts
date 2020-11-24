70fd9342e3d89620e4e7dc10f3654ff3972440f0
samm-w <sammw98@gmail.com>
2020-11-23 20:49:29 -0500

    Added all processes for when I create a new environment

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Scripts that I have made that I might use in the future
 
 ## Postinstall.sh
 To run, copy into a .sh file on your new install
-type chmod +x `filename`
+type `chmod +x filename`
 run the script with ` yes | sudo ./filename`
--- postinstall.sh
+++ postinstall.sh
@@ -6,3 +6,26 @@ apt-get update
 apt-get upgrade
 
 apt-get install nmap
+
+apt install nodejs
+apt install npm
+
+apt install software-properites-common
+add-apt-repository ppa:deadsnakes/ppa
+apt install python3
+apt install python3-dev python3-pip python3-venv
+
+apt-get install -y curl gpgv2 autoconf bison build-essential
+git-corelibapr1 postgresql libaprutil1 libcurl4openssl-dev libgmp3-dev libpcap-dev
+openssl libpq-dev libreadline6-dev libsqlite3-dev libssl-dev locate libsvn1 libtool
+libxml2 libxml2-dev libxslt-dev wget libyaml-dev ncurses-dev  postgresql-contrib xsel
+zlib1g zlib1g-dev
+
+curl https://raw.githubusercontent.com/rapid7/metasploit
+omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb
+> msfinstall && \
+
+chmod 755 msfinstall && \
+./msfinstall
+
+apt-get install git
